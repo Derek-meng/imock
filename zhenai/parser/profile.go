514f49d51daf5cd6336db91089631265b5992d67
Derek-meng <a0985265734@gmail.com>
2018-11-04 22:38:38 +0800

    add limit

--- zhenai/parser/profile.go
+++ zhenai/parser/profile.go
@@ -10,23 +10,23 @@ import (
 var ageRe = regexp.MustCompile(`<div class="m-btn purple" data-v-ff544c08="">([\d]+岁)</div>`)
 var allRE = regexp.MustCompile(`<div class="m-btn purple" data-v-ff544c08="">([^<]+)</div>`)
 
-func ParseProfile(contents [] byte,name string) engine.ParseResult {
+func ParseProfile(contents [] byte, name string) engine.ParseResult {
 	profile := model.Profile{}
-	profile.Name=name
-	age,err := strconv.Atoi(extractString(contents,ageRe))
-	if err!=nil {
-		profile.Age=age
+	profile.Name = name
+	age, err := strconv.Atoi(extractString(contents, ageRe))
+	if err != nil {
+		profile.Age = age
 	}
-	result:= engine.ParseResult{
-		Items:[]interface{}{profile},
+	result := engine.ParseResult{
+		Items: []interface{}{profile},
 	}
-	return  result
+	return result
 }
 func extractString(contents []byte, re *regexp.Regexp) string {
 	match := re.FindSubmatch(contents)
 	if len(match) >= 2 {
-		return  string(match[1])
-	}else {
+		return string(match[1])
+	} else {
 		return ""
 	}
 }
